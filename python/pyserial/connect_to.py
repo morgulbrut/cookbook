db3df0e75dd1db951e6ad4a66b2e83e8935ffe3d
Tillo <tillo.bosshart@gmail.com>
2020-05-29 16:38:53 +0200

    added pyboard example

--- python/pyserial/connect_to.py
+++ python/pyserial/connect_to.py
@@ -2,15 +2,26 @@
 
 import serial
 from serial.tools.list_ports import comports
+import time
 
+FTDI = 'VID:PID=0403:6001'
+CP2102 = 'VID:PID=10C4:EA60'
+PYBOARD = 'VID:PID=F055:9800'
 
-FTDI ='VID:PID=0403:6001'
-CP2102='VID:PID=10C4:EA60'
 
 if comports:
-    print('Connecting to FTDI')
     for port, desc, hwid in sorted(comports()):
         if FTDI in hwid:
-            print('found FTDI')
+            print('found FTDI on {}'.format(port))
             with serial.Serial(port, 115200, timeout=1) as ser:
                 ser.write(b'hello FTDI\n')
+        if PYBOARD in hwid:
+            print('found PYBPOARD on {}'.format(port))
+            with serial.Serial(port, 115200, timeout=1) as ser:
+                ser.write(b'set_led(1,1)\r\n')
+                ser.readline()
+                print(ser.readline())
+                time.sleep(1)
+                ser.write(b'set_led(1,0)\r\n')
+                ser.readline()
+                print(ser.readline())
